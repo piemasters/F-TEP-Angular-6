dd2091a6576dba2ffc701cf3f7c17333d0130f54
David <david.norton@cgi.com>
2018-09-03 17:48:39 +0100

    Moved config to environment, added initial helpdesk content

--- src/app/modules/helpdesk/helpdesk.component.ts
+++ src/app/modules/helpdesk/helpdesk.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-helpdesk',
@@ -6,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./helpdesk.component.css']
 })
 export class HelpdeskComponent implements OnInit {
+  ftepURL = environment.urls.ftepUrl;
+
+  videos = [
+    {
+      url: this.ftepURL + '/manual/search_create_databasket.mp4',
+      description: 'Perform a Search and create/manage databasket',
+      image: environment.assets.images + '/helpdesk/search.jpg'
+    }, {
+      url: this.ftepURL + '/manual/run_sentinel2_ndvi.mp4',
+      description: 'Run an NDVI service',
+      image: environment.assets.images + '/helpdesk/service_NDVI.jpg'
+    }, {
+      url: this.ftepURL + '/manual/open_ndvi_snap.mp4',
+      description: 'Open a product using Sentinel2 Toolbox',
+      image: environment.assets.images + '/helpdesk/NDVI_product_toolbox.jpg'
+    }, {
+      url: this.ftepURL + 'manual/vegindex_snap.mp4',
+      description: 'Create a VegetaionIndex and open result in Sentinel2 Toolbox',
+      image: environment.assets.images + '/helpdesk/vegind_product_toolbox.jpg'
+    }
+  ];
+
+  logo = environment.assets.images + '/logo.png';
 
   constructor() { }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  assets: {
+    images: '/src/assets/images'
+  },
+  urls: {
+    ftepUrl: 'https://forestry-tep.eo.esa.int',
+    ssoUrl: 'https://eo-sso-idp.evo-pdgs.com'
+  },
+  apiServer: {
+    apiUrl: 'http://158.234.89.204:8080/secure/api/v2.0'
+  },
+  tokens: {
+    mapboxToken: 'pk.eyJ1IjoidmFuemV0dGVucCIsImEiOiJjaXZiZTM3Y2owMDdqMnVwa2E1N2VsNGJnIn0.A9BNRSTYajN0fFaVdJIpzQ'
+  },
   production: false
 };
 
