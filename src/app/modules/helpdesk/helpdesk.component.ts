3d112eff1b41137c3c7dd07534352ad13ecc8f40
David <david.norton@cgi.com>
2018-09-05 09:52:02 +0100

    Helpdesk page tidyup

--- src/app/modules/helpdesk/helpdesk.component.ts
+++ src/app/modules/helpdesk/helpdesk.component.ts
@@ -7,29 +7,31 @@ import { environment } from '../../../environments/environment';
   styleUrls: ['./helpdesk.component.scss']
 })
 export class HelpdeskComponent implements OnInit {
-  ftepURL = environment.urls.ftepUrl;
+  assets = environment.assets;
+  urls = environment.urls;
+  ftepURL = this.urls.ftepUrl;
+
+  logoSrc = this.assets.images + '/logo.png';
 
   videos = [
     {
       url: this.ftepURL + '/manual/search_create_databasket.mp4',
       description: 'Perform a Search and create/manage databasket',
-      image: environment.assets.images + '/helpdesk/search.jpg'
+      image: this.assets.images + '/helpdesk/search.jpg'
     }, {
       url: this.ftepURL + '/manual/run_sentinel2_ndvi.mp4',
       description: 'Run an NDVI service',
-      image: environment.assets.images + '/helpdesk/service_NDVI.jpg'
+      image: this.assets.images + '/helpdesk/service_NDVI.jpg'
     }, {
       url: this.ftepURL + '/manual/open_ndvi_snap.mp4',
       description: 'Open a product using Sentinel2 Toolbox',
-      image: environment.assets.images + '/helpdesk/NDVI_product_toolbox.jpg'
+      image: this.assets.images + '/helpdesk/NDVI_product_toolbox.jpg'
     }, {
       url: this.ftepURL + 'manual/vegindex_snap.mp4',
       description: 'Create a VegetaionIndex and open result in Sentinel2 Toolbox',
-      image: environment.assets.images + '/helpdesk/vegind_product_toolbox.jpg'
+      image: this.assets.images + '/helpdesk/vegind_product_toolbox.jpg'
     }
   ];
-
-  logo = environment.assets.images + '/logo.png';
 
   constructor() { }
 
